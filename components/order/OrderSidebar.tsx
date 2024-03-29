d5838026ed38435fb322e16faddfd73a0d710c8f
LeninHernandez <90071666+lenin123333@users.noreply.github.com>
2024-03-29 13:39:24 -0600

    new file:   .env 	modified:   app/layout.tsx 	new file:   app/order/layout.tsx 	new file:   app/order/page.tsx 	new file:   components/order/OrderSidebar.tsx 	new file:   components/order/OrderSummary.tsx 	modified:   package-lock.json 	modified:   package.json 	new file:   prisma/schema.prisma

---
+++ components/order/OrderSidebar.tsx
@@ -0,0 +1,8 @@
+
+export default function OrderSidebar() {
+  return (
+    <aside className="p-5 md:w-72 md:h-screen bg-white">
+      OrderSidebar
+    </aside>
+  )
+}
---
+++ components/order/OrderSummary.tsx
@@ -0,0 +1,8 @@
+
+export default function OrderSummary() {
+  return (
+    <aside className="lg:h-screen lg:overflow-y-scroll bg-white md:w-64 lg:w-96 p-5">
+      <h1 className=" text-center text-4xl font-bold">Mi pedido</h1>
+    </aside>
+  )
+}
