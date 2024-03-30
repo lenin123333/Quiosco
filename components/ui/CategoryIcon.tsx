53e9e5a566b1f78f5967c9779473e1fc43ee31fd
LeninHernandez <90071666+lenin123333@users.noreply.github.com>
2024-03-29 18:23:03 -0600

    modified:   components/order/OrderSidebar.tsx 	new file:   components/ui/CategoryIcon.tsx 	modified:   package.json 	modified:   prisma/seed.ts 	new file:   public/icon_cafe.svg 	new file:   public/icon_dona.svg 	new file:   public/icon_galletas.svg 	new file:   public/icon_hamburguesa.svg 	new file:   public/icon_pastel.svg 	new file:   public/icon_pizza.svg 	new file:   public/logo.svg 	deleted:    public/next.svg 	new file:   public/products/cafe_01.jpg 	new file:   public/products/cafe_02.jpg 	new file:   public/products/cafe_03.jpg 	new file:   public/products/cafe_04.jpg 	new file:   public/products/cafe_05.jpg 	new file:   public/products/cafe_06.jpg 	new file:   public/products/cafe_07.jpg 	new file:   public/products/cafe_08.jpg 	new file:   public/products/cafe_09.jpg 	new file:   public/products/cafe_10.jpg 	new file:   public/products/cafe_11.jpg 	new file:   public/products/cafe_12.jpg 	new file:   public/products/cafe_13.jpg 	new file:   public/products/cafe_14.jpg 	new file:   public/products/donas_01.jpg 	new file:   public/products/donas_02.jpg 	new file:   public/products/donas_03.jpg 	new file:   public/products/donas_04.jpg 	new file:   public/products/donas_05.jpg 	new file:   public/products/donas_06.jpg 	new file:   public/products/donas_07.jpg 	new file:   public/products/donas_08.jpg 	new file:   public/products/donas_09.jpg 	new file:   public/products/donas_10.jpg 	new file:   public/products/donas_11.jpg 	new file:   public/products/donas_12.jpg 	new file:   public/products/donas_13.jpg 	new file:   public/products/donas_14.jpg 	new file:   public/products/galletas_01.jpg 	new file:   public/products/galletas_02.jpg 	new file:   public/products/galletas_03.jpg 	new file:   public/products/galletas_04.jpg 	new file:   public/products/galletas_05.jpg 	new file:   public/products/galletas_06.jpg 	new file:   public/products/hamburguesas_01.jpg 	new file:   public/products/hamburguesas_02.jpg 	new file:   public/products/hamburguesas_03.jpg 	new file:   public/products/hamburguesas_04.jpg 	new file:   public/products/hamburguesas_05.jpg 	new file:   public/products/hamburguesas_06.jpg 	new file:   public/products/hamburguesas_07.jpg 	new file:   public/products/hamburguesas_08.jpg 	new file:   public/products/pastel_01.jpg 	new file:   public/products/pastel_02.jpg 	new file:   public/products/pastel_03.jpg 	new file:   public/products/pastel_04.jpg 	new file:   public/products/pastel_05.jpg 	new file:   public/products/pastel_06.jpg 	new file:   public/products/pizzas_01.jpg 	new file:   public/products/pizzas_02.jpg 	new file:   public/products/pizzas_03.jpg 	new file:   public/products/pizzas_04.jpg 	new file:   public/products/pizzas_05.jpg 	new file:   public/products/pizzas_06.jpg 	new file:   public/products/pizzas_07.jpg 	new file:   public/products/pizzas_08.jpg 	new file:   public/products/pizzas_09.jpg 	new file:   public/products/pizzas_10.jpg 	new file:   public/products/pizzas_11.jpg 	deleted:    public/vercel.svg 	new file:   src/lib/prisma.ts

---
+++ components/ui/CategoryIcon.tsx
@@ -0,0 +1,18 @@
+import { Category } from "@prisma/client"
+import Image from "next/image"
+type CategoryIconProps = {
+  category: Category
+}
+export default function CategoryIcon({ category }: CategoryIconProps) {
+  return (
+    <div className={` flex items-center gap-4 w-full border-t border-gray-200 p-3
+       last-of-type:border-b`}>
+      <div className="w-16 h-16 relative">
+        <Image fill
+          src={`/icon_${category.slug}.svg`}
+          alt="Imagen Categoria" />
+      </div>
+      <p className=" text-xl font-bold">{category.name}</p>
+    </div>
+  )
+}
