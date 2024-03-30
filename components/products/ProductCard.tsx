680abc7dedf340a80871269c9073b73f0e62f17e
LeninHernandez <90071666+lenin123333@users.noreply.github.com>
2024-03-29 19:48:36 -0600

    modified:   components/products/ProductCard.tsx

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -10,7 +10,7 @@ type ProductCardProps = {
 export default function ProductCard({ product }: ProductCardProps) {
     return (
         <div className=' border bg-white'>
-            <Image  src={`/products/${product.image}.jpg`} alt={`Imagen Platillo ${product.name}`} />
+            <Image width={400} height={500}  src={`/products/${product.image}.jpg`} alt={`Imagen Platillo ${product.name}`} />
             <div className='p-5'>
                 <h3 className=' text-2xl font-bold'>{product.name}</h3>
                 <p className=' mt-5 font-black text-4xl text-amber-500'>
